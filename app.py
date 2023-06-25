987fda57ff869e0150eb128a58ab651ba3d04df7
Emirinho22 <137709044+Emirinho22@users.noreply.github.com>
2023-06-25 19:48:45 +0300

    Değişkenler

--- app.py
+++ app.py
@@ -1,1 +1,87 @@
 print("Hello, World!")
+"""
+print("Hello word")
+
+
+# Python Indentation (Tab Bırak)
+if 5 > 3:
+    print("5 büyüktür 3")
+
+    
+# Değişkenler 
+
+x = 7 # x'e 7 verildi
+y = 1
+print(x + y)
+
+kullaniciAdi = "Emir"
+# kullaniciSifre = "1234ab.#'$"
+
+kullaniciSifre = 123123213
+
+print(type(kullaniciSifre))
+
+"""
+
+
+"""
+# Casting - çevirme işlemi
+x = str(3)
+print(x)
+
+
+sifre = int("1234")
+print(sifre)
+print(type(sifre))
+
+"""
+
+
+x = 7 # x'e 7 verildi
+X = 77 # X'e 77 verildi
+y = 1
+print(x + y)
+
+sinifMevcudu = 20
+yeniSinifMevcudu = sinifMevcudu + 1
+print(yeniSinifMevcudu)
+
+
+# kontrol tuşları
+x, y = 2, 5
+
+w = "ileri"
+a = "sol"
+s = "geri"
+d = "sağa"
+c = "eğil"
+
+w, a, s, d = "ileri", "sol", "geri", "sağa"
+
+
+kucukButon = ortaButon = buyukButon = "Turuncu"
+
+adim = "Emir"
+soyAdim = "Şafak"
+
+print(adim, soyAdim)
+print(adim + soyAdim)
+print(adim + " " + soyAdim)
+# string
+# integer
+
+a = 5
+b = 4
+# kod ekleeyeceğiz
+# nasıl string yaparız ?
+
+# çıktısı 54 -> 5 4 
+print(a, b) # + için -> stringse eyvallah amaaa integer vermişsen TİPLERE dikkat et
+ 
+x = 5
+y = 10
+toplam = x + y
+print(toplam)
+
+
+x = y = "Orange"
